74b30cf53148e8542238d90b46e3491d0c958cb6
mcdavis705 <mcdavis705@gmail.com>
2025-11-12 06:03:02 -0500

    Declare slice functions

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,24 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+    const {name, image, cost} = action.payload;
+    const existingitem = state.items.find(item => item.name === name);
+    if (existingitem) {
+        existingitem.updateQuantity++;
+    } else {
+        state.items.push({name, image, cost, quantity: 1})
+    }
     },
     removeItem: (state, action) => {
+    state.items = state.items.filter(item => item.name !== action.payload);
+
     },
     updateQuantity: (state, action) => {
-
-    
+    const {name, quantity} = action.payload;
+    itemToUpdate = state.items.find(item => item.name === name);
+    if(itemToUpdate){
+        itemToUpdate.quantity = quantity;
+    }
     },
   },
 });
